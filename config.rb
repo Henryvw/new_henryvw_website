cca26699aa2c3ad5bd502894fb61636080d20d80
Henry van Wagenberg <henryvw1@gmail.com>
2020-05-26 12:21:05 +0200

    Update slim config html5 => html

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-set :slim, { :format => :html5 }
+set :slim, { :format => :html }
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
